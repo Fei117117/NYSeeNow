bb0cf56dfc20517eb2aba32f5db43debbdbb6467
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-06-23 10:50:37 +0100

    changing vue library

--- NYSeeNow-frontend/src/net/net.js
+++ NYSeeNow-frontend/src/net/net.js
@@ -1,32 +1,35 @@
-import axios from "axios";
-import {ElMessage} from "element-plus"
+import axios from 'axios'
+// import { ElMessage } from 'element-plus'
+import { toast } from 'react-toastify'
 
-const defaultError = () => ElMessage.error('Error!')
-const defaultFailure = (message) => ElMessage.warning(message)
+const defaultError = () => toast.error('Error!')
+const defaultFailure = (message) => toast.warning(message)
 
-function post(url, data, success, failure = defaultFailure, error = defaultError){
-    axios.post(url, data, {
-        headers:{
-            'Content-Type':'application/x-www-form-urlencoded'
-        },
-        withCredentials:true
-    }).then(({data}) => {
-        if(data.success)
-            success(data.message, data.status)
-        else
-            failure(data.message, data.status)
-    }).catch(error)
+function post(url, data, success, failure = defaultFailure, error = defaultError) {
+  axios
+    .post(url, data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      withCredentials: true
+    })
+    .then(({ data }) => {
+      if (data.success) success(data.message, data.status)
+      else failure(data.message, data.status)
+    })
+    .catch(error)
 }
 
-function get(url, success, failure = defaultFailure, error = defaultError){
-    axios.get(url, {
-        withCredentials:true
-    }).then(({data}) => {
-        if(data.success)
-            success(data.message, data.status)
-        else
-            failure(data.message, data.status)
-    }).catch(error)
+function get(url, success, failure = defaultFailure, error = defaultError) {
+  axios
+    .get(url, {
+      withCredentials: true
+    })
+    .then(({ data }) => {
+      if (data.success) success(data.message, data.status)
+      else failure(data.message, data.status)
+    })
+    .catch(error)
 }
 
-export{get, post}
+export { get, post }
