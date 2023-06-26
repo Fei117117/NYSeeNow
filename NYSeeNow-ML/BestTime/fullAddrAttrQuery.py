672812d0071d0ce9ab66e8e773bbb89b1a6c75bf
Cian Belton <cianbelton@protonmail.com>
2023-06-26 12:27:02 +0100

    best time queries

--- NYSeeNow-ML/BestTime/fullAddrAttrQuery.py
+++ NYSeeNow-ML/BestTime/fullAddrAttrQuery.py
@@ -13,22 +13,25 @@ url = "https://besttime.app/api/v1/forecasts"
 responses = []  # List to store the responses
 
 
-start=0
-end=20
+start=420
+end=1092
 #the number of venues (end-start)/2
 step=2
 #left misspelled on purpose to avoid accidental queries
-private_k='pri_b6c22e039524443298cc7c4ed37a6c9a'
+private_key='pri_b6c22e039524443298cc7c4ed37a6c9a'
 
 for i in range(start, end, step):
     params = {
-        'api_key_private': private_key,
+        'api_key_private': private_k,
         'venue_name': data[i],
         'venue_address': data[i + 1],
     }
     response = requests.request("POST", url, params=params)
     responses.append(response.json())
+    # Write each response to a JSON file immediately after receiving it
+    with open(f'BestTime/responses_210_547.json', 'a') as file:
+        json.dump(response.json(), file)
 
 # Write the responses to a JSON file
-with open('BestTime/fullAddr_1_10.json', 'w') as file:
+with open('BestTime/fullAddr_210_547.json', 'w') as file:
     json.dump(responses, file)
