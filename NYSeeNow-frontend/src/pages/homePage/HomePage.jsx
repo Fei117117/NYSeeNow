550656981817eb90e63a1f8349e13bcff717d933
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-06-28 17:24:57 +0100

    marker render bug fix

--- NYSeeNow-frontend/src/pages/homePage/HomePage.jsx
+++ NYSeeNow-frontend/src/pages/homePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useMemo } from 'react'
 
 import markerData from '../../assets/best_time_ok_data.json'
 
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
+import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api'
 
 function fetch_marker_data() {
   let coordinate_json = []
@@ -25,6 +25,16 @@ function fetch_marker_data() {
 }
 
 export const HomePage = () => {
+  const fetched_positions_array = fetch_marker_data()
+
+  const center_point = useMemo(() => ({ lat: 40.7484, lng: -73.9857 }), [])
+
+  const [mapLoaded, setMapLoaded] = useState(false)
+
+  const handleMapLoading = () => {
+    setMapLoaded(true)
+  }
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCTCIyYHCeWx1duTYP38_g8ikK3_fwVrSE'
   })
@@ -32,16 +42,22 @@ export const HomePage = () => {
   if (!isLoaded) {
     return <h1>Map loading</h1>
   } else {
-    return <Map />
+    return (
+      <GoogleMap
+        mapContainerClassName="map-container"
+        mapTypeId="roadmap"
+        zoom={15}
+        center={center_point}
+        onLoad={handleMapLoading}
+      >
+        {mapLoaded &&
+          fetched_positions_array.map((point, index) => <MarkerF key={index} position={point} />)}
+      </GoogleMap>
+    )
   }
 }
 
 function Map() {
-  const [mapLoaded, setMapLoaded] = useState(false)
-
-  const handleMapLoad = () => {
-    setMapLoaded(true)
-  }
   const fetched_positions_array = fetch_marker_data()
 
   const center_point = useMemo(() => ({ lat: 40.7484, lng: -73.9857 }), [])
@@ -51,10 +67,10 @@ function Map() {
       mapTypeId="roadmap"
       zoom={15}
       center={center_point}
-      onLoad={handleMapLoad}
     >
-      {mapLoaded &&
-        fetched_positions_array.map((point, index) => <Marker key={index} position={point} />)}
+      {fetched_positions_array.map((point, index) => (
+        <Marker key={index} position={point} />
+      ))}
     </GoogleMap>
   )
 }
