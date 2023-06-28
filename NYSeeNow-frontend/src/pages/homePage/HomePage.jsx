1b277287977bf9d06a63cbfc75cfcbad981c7b07
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-06-28 16:19:51 +0100

    Adding the sample dataset and the display

--- NYSeeNow-frontend/src/pages/homePage/HomePage.jsx
+++ NYSeeNow-frontend/src/pages/homePage/HomePage.jsx
@@ -1,12 +1,30 @@
 import { useAuth } from '../../context/AuthContext'
 
-import { useEffect } from 'react'
+import { useEffect, useState, useMemo } from 'react'
+
+import markerData from '../../assets/best_time_ok_data.json'
 
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
 
-export const HomePage = () => {
-  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth()
+function fetch_marker_data() {
+  let coordinate_json = []
+  for (let key in markerData) {
+    if (markerData.hasOwnProperty(key)) {
+      let marker = markerData[key]
+      let coordinates_string = marker['Venue Coordinates']
+      let position = coordinates_string.split(' ')
+      let marker_coordinates = {
+        lat: parseFloat(position[0]),
+        lng: parseFloat(position[1])
+      }
+      coordinate_json.push(marker_coordinates)
+    }
+  }
 
+  return coordinate_json
+}
+
+export const HomePage = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCTCIyYHCeWx1duTYP38_g8ikK3_fwVrSE'
   })
@@ -19,12 +37,24 @@ export const HomePage = () => {
 }
 
 function Map() {
+  const [mapLoaded, setMapLoaded] = useState(false)
+
+  const handleMapLoad = () => {
+    setMapLoaded(true)
+  }
+  const fetched_positions_array = fetch_marker_data()
+
+  const center_point = useMemo(() => ({ lat: 40.7484, lng: -73.9857 }), [])
   return (
     <GoogleMap
       mapContainerClassName="map-container"
       mapTypeId="roadmap"
-      zoom={9}
-      center={{ lat: 40.7484, lng: -73.9857 }}
-    ></GoogleMap>
+      zoom={15}
+      center={center_point}
+      onLoad={handleMapLoad}
+    >
+      {mapLoaded &&
+        fetched_positions_array.map((point, index) => <Marker key={index} position={point} />)}
+    </GoogleMap>
   )
 }
