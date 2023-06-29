a3f3d46f40f249d362e0b0e28a2ade13acc4964f
Cian Belton <cianbelton@protonmail.com>
2023-06-29 15:12:03 +0100

    weather for the day and subway work

--- NYSeeNow-ML/OpenWeather/apiCallOpenW.py
+++ NYSeeNow-ML/OpenWeather/apiCallOpenW.py
@@ -8,9 +8,8 @@ import time
 #start the time on the 1/1/2019
 #make sure to start from the most recent day called
 start_time=1546300800
-start_time=1549130400+3600
-p=0
-start_time=start_time+3456000*p
+#edit below with last time called in weather_data2 file
+start_time=1552629600+3600
 #each_day adds 3456000 seconds to the start time
 #one hour in unix time is 3600 sec
 hour=3600
@@ -26,7 +25,15 @@ for j in range(1,17):
         current_time=start+hour*i
         print(current_time)
         # Connect to OpenWeather API
-        r = requests.get(owLogin.owUrl, params={'lat': owLogin.owLat, 'lon': owLogin.owLon, 'dt': current_time, 'appid': owLogin.owKey })
+        try:
+            r = requests.get(owLogin.owUrl, params={'lat': owLogin.owLat, 'lon': owLogin.owLon, 'dt': current_time, 'appid': owLogin.owKey }, verify=False)
+        
+        except SSLError:
+            print('SSL Error, will retry')
+            time.sleep(1)
+            #try again
+            r = requests.get(owLogin.owUrl, params={'lat': owLogin.owLat, 'lon': owLogin.owLon, 'dt': current_time, 'appid': owLogin.owKey }, verify=False)
+            continue
 
         if r.status_code == 200:
             # If connection successful:
@@ -37,6 +44,5 @@ for j in range(1,17):
             with open('OpenWeather/weatherData_2.json', 'a') as file:
                 json.dump(data, file)
                             
-        
     #wait for 61 seconds before repeating, keeps limit to 60 calls per second
     time.sleep(61)
