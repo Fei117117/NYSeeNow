89bd913ae80db757ef7dfc16ba63f83e082ee8b9
Cian Belton <cianbelton@protonmail.com>
2023-07-05 11:51:20 +0100

    delta

--- NYSeeNow-ML/OpenWeather/apiCallOpenW.py
+++ NYSeeNow-ML/OpenWeather/apiCallOpenW.py
@@ -9,7 +9,7 @@ import time
 #make sure to start from the most recent day called
 start_time=1546300800
 #edit below with last time called in weather_data2 file
-start_time=1563465600+3600
+start_time=1566921600+3600
 #one hour in unix time is 3600 sec
 hour=3600
 #end time is 1/1/2020
@@ -40,7 +40,7 @@ for j in range(1,17):
             data = r.json()
 
             #send data to a json file
-            with open('OpenWeather/weatherData_2.json', 'a') as file:
+            with open('OpenWeather/weatherData.json', 'a') as file:
                 json.dump(data, file)
                             
     #wait for 61 seconds before repeating, keeps limit to 60 calls per second
