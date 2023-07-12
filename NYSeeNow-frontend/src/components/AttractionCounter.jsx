692438c479a36f92a5a62bc85298728dbb89df08
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-07-12 15:55:45 +0100

    Fixed attractions counter, now moved with sidebar

--- NYSeeNow-frontend/src/components/AttractionCounter.jsx
+++ NYSeeNow-frontend/src/components/AttractionCounter.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useSelection } from '../context/SelectionContext';
 
-const AttractionCounter = () => {
+const AttractionCounter = ({ left }) => {
   const { selectedList } = useSelection();
 
+  // Use the 'left' prop to position the counter
+  const counterStyle = {
+    left,
+  };
+
   return (
-    <div className="attraction-counter">
+    <div className="attraction-counter" style={counterStyle}>
       <p>{selectedList ? selectedList.length : 0}</p>
     </div>
   );
--- NYSeeNow-frontend/src/components/SideBar.jsx
+++ NYSeeNow-frontend/src/components/SideBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SidebarContent from './SidebarContent';
-import AttractionCounter from './AttractionCounter'; // import the counter
+import AttractionCounter from './AttractionCounter';
 
 export const SideBar = ({ isOpen, setIsOpen }) => {
+  // State for holding the left position of AttractionCounter
+  const [counterLeft, setCounterLeft] = useState('60px');
+
+  useEffect(() => {
+    setCounterLeft(isOpen ? 'calc(50% + 60px)' : '60px'); // update the position according to sidebar's state
+  }, [isOpen]);
+
   return (
     <aside className={`side-bar ${isOpen ? 'open' : ''}`}>
       <div className="sidebar-icons">
@@ -16,9 +23,9 @@ export const SideBar = ({ isOpen, setIsOpen }) => {
         </div>
       </div>
       <div className="sidebar-content">
-        {isOpen && <SidebarContent />} {/* Includes the SidebarContent component */}
+        {isOpen && <SidebarContent />}
       </div>
-      <AttractionCounter /> {/* Add the AttractionCounter component here */}
+      <AttractionCounter left={counterLeft} />
     </aside>
   );
 };
