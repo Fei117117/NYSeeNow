796c2b96acdf48f3cbc52f0ae34a0fa2761a53d4
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-07-25 18:45:12 +0100

    saved trips showing up

--- NYSeeNow-frontend/src/pages/savedItinerariesPage/TripContainer.jsx
+++ NYSeeNow-frontend/src/pages/savedItinerariesPage/TripContainer.jsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import styles from './TripContainer.module.css'
 
 export const TripContainer = (props) => {
+  const [tripObj, setTripObj] = useState(null)
   useEffect(() => {
     const trip_detail_string = props.trip_details
-    console.log('My Trip Details are:', trip_detail_string)
+    setTripObj(JSON.parse(trip_detail_string))
   }, [])
 
-  return <div>Somethings</div>
+  if (tripObj) {
+    return (
+      <div className={styles.tripContainer}>
+        <div>Trip Id: {tripObj['trip_id']}</div>
+        <div>Number of Attractions: {tripObj['number_of_attractions']}</div>
+        <div>Start Date: {tripObj['start_date']}</div>
+        <div>End Date: {tripObj['end_date']}</div>
+      </div>
+    )
+  } else {
+    return <div>Nothing in this.</div>
+  }
 }
