ba5ecb4ab9094dd10b6027e3c2b820f1d4cf887e
Fei117117 <katherinekao@icloud.com>
2023-07-28 00:20:00 +0100

    delete trip

--- NYSeeNow-frontend/src/pages/savedItinerariesPage/TripContainer.jsx
+++ NYSeeNow-frontend/src/pages/savedItinerariesPage/TripContainer.jsx
@@ -1,20 +1,50 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './TripContainer.module.css'
+import axios from 'axios';
+import { useAuth } from '../../context/AuthContext'
 
 export const TripContainer = (props) => {
   const [tripObj, setTripObj] = useState(null)
-  useEffect(() => {
+  const { authUser } = useAuth();
+
+    useEffect(() => {
     const trip_detail_string = props.trip_details
     setTripObj(JSON.parse(trip_detail_string))
   }, [])
 
-  if (tripObj) {
+    const handleDelete = async () => {
+        try {
+            const username = tripObj?.username;
+
+            if (!username) {
+                console.error("Username is undefined");
+                return;
+            }
+
+            const response = await axios.delete(`/trip/delete/${username}/${tripObj['trip_id']}`);
+
+            if (response.status === 200) {
+                // Refresh the component or do something after the trip is deleted
+                console.log("Trip deleted successfully");
+            } else {
+                console.error("Failed to delete trip");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+
+
+    if (tripObj) {
     return (
       <div className={styles.tripContainer}>
         <div>Trip Id: {tripObj['trip_id']}</div>
         <div>Number of Attractions: {tripObj['number_of_attractions']}</div>
         <div>Start Date: {tripObj['start_date']}</div>
         <div>End Date: {tripObj['end_date']}</div>
+          <button className="card-delete-button-trip" onClick={() => { console.log(authUser); handleDelete(); }}>Delete</button>
       </div>
     )
   } else {
