8215dc54b1ea9c2764be7452742963117fbdb80c
Fei117117 <katherinekao@icloud.com>
2023-07-31 16:10:19 +0100

    add user info to user profile

--- NYSeeNow-frontend/src/pages/userProfilePage/UserProfile.jsx
+++ NYSeeNow-frontend/src/pages/userProfilePage/UserProfile.jsx
@@ -2,15 +2,27 @@ import { useAuth } from '../../context/AuthContext'
 import { useState } from 'react'
 import { AuthOverlay } from '../authOverlay/AuthOverlay'
 import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
+
 
 export const UserProfile = () => {
   const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth()
+  const [email, setEmail] = useState('')
+
+
+  useEffect(() => {
+    axios.get(`/api/users/${authUser}`)  // replace this with your API endpoint
+        .then(response => setEmail(response.data.email))
+        .catch(error => console.error('Error fetching user data:', error))
+  }, [authUser])
 
   const toggleForm = (formName) => {
     setCurrentForm(formName)
   }
   // Get the state, if login not done, show overlay. I login, render userprofile page just like that.
   const navigate = useNavigate()
+
   const logoutHandler = () => {
     setIsLoggedIn(false)
     navigate('/')
@@ -19,6 +31,8 @@ export const UserProfile = () => {
     return (
       <>
         <h1>My UserProfile</h1>
+        <p>Username: {authUser}</p>
+        <p>Email: {email}</p>
         <button onClick={logoutHandler}>Logout</button>
       </>
     )
