2357c94d3d66f0bd9967b751a1ad6848db996f4b
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-08-01 17:46:48 +0100

    Improved styling, buttons etc.

--- NYSeeNow-frontend/src/pages/LandingPage/LandingPage.jsx
+++ NYSeeNow-frontend/src/pages/LandingPage/LandingPage.jsx
@@ -3,18 +3,14 @@ import styles from './LandingPage.module.css';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-// import AppUsageImg1 from './images/appUsageImg1.jpg';
-// import AppUsageImg2 from './images/appUsageImg2.jpg';
-// import AppUsageImg3 from './images/appUsageImg3.jpg';
-
 const LandingPage = () => {
     const location = useLocation();
     return (
         <div className={styles['landing-page']}>
             <header>
-            <nav className={location.pathname === "/" ? styles['landing-navbar'] : ""}>
+                <nav className={location.pathname === "/" ? styles['landing-navbar'] : ""}>
                     <div className={styles['logo-section']}>
-
+                        <img src="nyseenowLogoLanding.png" alt="logo" style={{ maxHeight: '50px', maxWidth: '100%' }} />
                         <h1>NYSeeNOW</h1>
                     </div>
                     <div className={styles['nav-links']}>
@@ -25,11 +21,13 @@ const LandingPage = () => {
                 </nav>
             </header>
             <section className={styles.hero}>
-            <img src="NYSeeLogo.png" alt="hero logo" className={styles['hero-logo']} />
+                <h1 className={styles['hero-heading']}>Live your NYC dream trip, NOW!</h1>
+                <p className={styles['hero-subheading']}>Find top attractions, build an itinerary, and decide what suits you here and NOW. Using our 'NOW mode' you can check busyness levels, weather info and directions between destinations. All in one place here and now!</p>
                 <h2 className={styles.slogan}>The best time is now</h2>
                 <div className={styles['action-buttons']}>
-                    <Link to="/home" className={styles.button}>Plan a trip</Link>
-                    <Link to="/explore" className={styles.button}>Explore now!</Link>
+                    <Link to="/home" className={`${styles.button} ${styles['button-plan']}`}>Plan a trip</Link>
+                    <Link to="/explore" className={`${styles.button} ${styles['button-explore']}`}>Explore NOW!</Link>
+
                 </div>
             </section>
             <section className={styles['app-usage']}>
