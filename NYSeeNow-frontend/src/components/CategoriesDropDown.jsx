0112308f0d5a6f9e8f620de8c3a878595ab889f8
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-08-08 23:41:10 +0100

    Updated categories, features colour codes and symmbols to cater for colour blind individuals

--- NYSeeNow-frontend/src/components/CategoriesDropDown.jsx
+++ NYSeeNow-frontend/src/components/CategoriesDropDown.jsx
@@ -4,6 +4,17 @@ import options from '../assets/attraction_options.json';
 import { useCategories } from '../context/CategoriesContext';
 import { getTypeColor } from '../pages/homePage/HomePageMarker';
 
+
+const attractionSVG = "/attractions.svg";
+const museumSVG = "/museums.svg";
+const artworkSVG = "/artworks.svg";
+const aquariumSVG = "/aquariums.svg";
+const campsiteSVG = "/campsites.svg";
+const gallerySVG = "/galleries.svg";
+const sightseeingSVG = "/sightseeing.svg";
+
+
+
 const CategoryDropDown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { selectedOptions, setSelectedOptions } = useCategories();
@@ -22,6 +33,34 @@ const CategoryDropDown = () => {
     }
   };
 
+
+  const getSVGIcon = (type) => {
+    const icons = {
+      attraction: attractionSVG,
+      museum: museumSVG,
+      artwork: artworkSVG,
+      aquarium: aquariumSVG,
+      camp_site: campsiteSVG,
+      gallery: gallerySVG,
+      sightseeing: sightseeingSVG
+    };
+    return icons[type] || attractionSVG; // default to attractionSVG if type not found
+  };
+
+  const getSvgPath = (type) => {
+    const svgMap = {
+      attraction: "/attractions.svg",
+      museum: "/museums.svg",
+      artwork: "/artworks.svg",
+      aquarium: "/aquariums.svg",
+      camp_site: "/campsites.svg",
+      gallery: "/galleries.svg",
+      sightseeing: "/sightseeing.svg"
+    };
+  
+    return svgMap[type] || "/attractions.svg"; // default to attractions.svg if type is not found
+  };
+
   return (
     <div className={styles.categoryBlock}>
       <a onClick={toggleDropdown}>Categories</a>
@@ -29,6 +68,8 @@ const CategoryDropDown = () => {
         <div className={styles.dropdownContainer}>
           {options.map((option) => (
             <label key={option.id} className={styles.optionLabel}>
+              {/* Add SVG to the label */}
+              <img src={getSvgPath(option.types[0])} alt={option.label} className={styles.svgIcon} />
               <span
                 className={styles.colorCircle}
                 style={{ backgroundColor: getTypeColor(option.types[0]) }}
@@ -45,6 +86,6 @@ const CategoryDropDown = () => {
       )}
     </div>
   );
-};
-
-export default CategoryDropDown;
+}; 
+  
+export default CategoryDropDown;  
