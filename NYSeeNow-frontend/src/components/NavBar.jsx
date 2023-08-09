13636fdc1f6c5467b4a9e58376b0f8ec2068fbdc
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-08-09 18:16:45 +0100

    Added placeholder profile pic to navbar

--- NYSeeNow-frontend/src/components/NavBar.jsx
+++ NYSeeNow-frontend/src/components/NavBar.jsx
@@ -1,25 +1,26 @@
-import { Link } from 'react-router-dom'
-import { SearchBar } from './SearchBar'
-import CategoryDropDown from './CategoriesDropDown'
-import styles from './NavBar.module.css'
-import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { SearchBar } from './SearchBar';
+import CategoryDropDown from './CategoriesDropDown';
+import styles from './NavBar.module.css';
+import { useState } from 'react';
 
 export const NavBar = ({ isOpen, set_map_center }) => {
   const navStyle = {
     marginLeft: isOpen ? '40%' : '0', // position of sidebar extended
     transition: 'margin-left 0.3s ease' // transition animation
-  }
+  };
 
   const logoStyle = {
     marginLeft: isOpen ? '50%' : '0', // position of logo when sidebar is extended
     transition: 'margin-left 0.3s ease' // transition animation
-  }
+  };
 
-  const [burgerOpen, setBurgerOpen] = useState(false)
+  const [burgerOpen, setBurgerOpen] = useState(false);
 
   const toggleBurger = () => {
-    setBurgerOpen(!burgerOpen)
-  }
+    setBurgerOpen(!burgerOpen);
+  };
+
   return (
     <nav style={navStyle}>
       <ul>
@@ -35,25 +36,32 @@ export const NavBar = ({ isOpen, set_map_center }) => {
           </Link>
         </div>
       </ul>
-      <ul>
-        <li>
-          <SearchBar set_map_center={set_map_center}></SearchBar>
-        </li>
-      </ul>
-      <ul className={styles.changeableNav}>
-        <li>
-          <CategoryDropDown></CategoryDropDown>
-        </li>
-        <li>
-          <Link to="/userprofile">User</Link>
-        </li>
-      </ul>
+      <div className={styles.navItemsContainer}>
+        <ul>
+          <li>
+            <SearchBar set_map_center={set_map_center}></SearchBar>
+          </li>
+        </ul>
+        <ul className={styles.changeableNav}>
+          <li>
+            <CategoryDropDown></CategoryDropDown>
+          </li>
+          <li>
+            <Link to="/userprofile">User</Link>
+          </li>
+        </ul>
+      </div>
       <ul className={styles.navHamburger}>
         <li onClick={toggleBurger}>
           <i className="fas fa-bars"></i>
         </li>
         {burgerOpen && (
           <div className={styles.dropdownContainer}>
+            <div className={styles.dropdownImageContainer}>
+              <Link to="/userprofile">
+              <img src="placeholder.png" alt="Placeholder" className={styles.dropdownPlaceholderImage} />
+              </Link>
+            </div>
             <div>
               <CategoryDropDown></CategoryDropDown>
             </div>
@@ -62,7 +70,13 @@ export const NavBar = ({ isOpen, set_map_center }) => {
             </div>
           </div>
         )}
+
       </ul>
+      <div className={styles.dropdownImageContainer}>
+        <Link to="/userprofile">
+          <img src="placeholder.png" alt="Placeholder" className={styles.dropdownPlaceholderImage} />
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
