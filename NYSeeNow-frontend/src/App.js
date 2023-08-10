45a26349e9e28d51d10f028dc3d2bb33722b3902
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-08-10 02:22:59 +0100

    map position changes

--- NYSeeNow-frontend/src/App.js
+++ NYSeeNow-frontend/src/App.js
@@ -19,15 +19,14 @@ import { useLocation } from 'react-router-dom'
 import TripEditPage from './pages/itineraryBuilderPage/ItineraryEdit'
 import { Register } from './pages/authOverlay/Register'
 import ItineraryEdit from './pages/itineraryBuilderPage/ItineraryEdit'
-import { OpenCardProvider } from './context/OpenCardProvider';
+import { OpenCardProvider } from './context/OpenCardProvider'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
-  const [center, setMapCenter] = useState({ lat: 40.7484, lng: -73.9857 })
-  const [zoom, setZoom] = useState(10) // Add this new state
+  const [center, setMapCenter] = useState({ lat: 40.7699, lng: -73.9757 })
+  const [zoom, setZoom] = useState(9) // Add this new state
   const [showLocator, setShowLocator] = useState(false)
   const location = useLocation()
-
 
   return (
     <>
@@ -37,35 +36,36 @@ function App() {
             <LocatorContext.Provider value={{ showLocator, setShowLocator }}>
               <TripDataProvider>
                 <OpenCardProvider>
-                {location.pathname !== '/' && <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />}
-                {location.pathname !== '/' && <NavBar isOpen={isOpen} set_map_center={setMapCenter} />}
-            
-                
-                <div className="route-container">
-                  <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route
-                      path="/home"
-                      element={
-                        <HomePage
-                          map_center={center}
-                          setMapCenter={setMapCenter}
-                          zoom={zoom}
-                          setZoom={setZoom}
-                        />
-                      }
-                    />
-                    <Route path="/itineraries" element={<MyItineraries />} />
-                    <Route path="/itinerary-builder" element={<ItineraryBuilder />} />
-                    <Route path="/userprofile" element={<UserProfile />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route
-                      path="/attractions-map"
-                      element={<HomePage map_center={center} setMapCenter={setMapCenter} />}
-                    />
-                    <Route path="/ItineraryEdit" element={<ItineraryEdit />} />
+                  {location.pathname !== '/' && <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />}
+                  {location.pathname !== '/' && (
+                    <NavBar isOpen={isOpen} set_map_center={setMapCenter} />
+                  )}
+
+                  <div className="route-container">
+                    <Routes>
+                      <Route path="/" element={<LandingPage />} />
+                      <Route
+                        path="/home"
+                        element={
+                          <HomePage
+                            map_center={center}
+                            setMapCenter={setMapCenter}
+                            zoom={zoom}
+                            setZoom={setZoom}
+                          />
+                        }
+                      />
+                      <Route path="/itineraries" element={<MyItineraries />} />
+                      <Route path="/itinerary-builder" element={<ItineraryBuilder />} />
+                      <Route path="/userprofile" element={<UserProfile />} />
+                      <Route path="/register" element={<Register />} />
+                      <Route
+                        path="/attractions-map"
+                        element={<HomePage map_center={center} setMapCenter={setMapCenter} />}
+                      />
+                      <Route path="/ItineraryEdit" element={<ItineraryEdit />} />
                     </Routes>
-                </div>
+                  </div>
                 </OpenCardProvider>
               </TripDataProvider>
             </LocatorContext.Provider>
